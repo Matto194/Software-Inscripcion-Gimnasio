7edbf522ce2a28023b91f4387496875f902c4581
Matto194 <matias.ojedas@usm.cl>
2023-05-31 00:29:00 -0400

    Pagina_Lista

--- Software_Inscripcion/core/models.py
+++ Software_Inscripcion/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=30)
+    rut= models.CharField(max_length=30)
+    correo = models.EmailField()
+
+    def get_full_name(self):
+        return f'{self.nombre} {self.apellido}'
--- Software_Inscripcion/core/views.py
+++ Software_Inscripcion/core/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .forms import UsuarioForm
+from django.urls import reverse
+from .models import Usuario
 
 # Create your views here.
 @login_required
 def home(request):
-    return render(request,'core/base.html')
+    usuario_form = UsuarioForm()
+    usuarios = Usuario.objects.all()
+
+    if request.method == 'POST':
+        usuario_form = UsuarioForm(data=request.POST)
+
+        if usuario_form.is_valid():
+            usuario_form.save()
+            return redirect(reverse('home')+'?ok')
+        else:
+            return redirect(reverse('home')+'?error') 
+
+
+    return render(request,'core/base.html', {'form':usuario_form,'usuarios':usuarios})
 
 def prueba(request):
     return render(request, 'core/home.html')
+
