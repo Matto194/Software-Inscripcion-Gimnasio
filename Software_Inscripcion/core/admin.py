dc563f1b98230f65f31bd996186cf562665c6207
Matto194 <matias.ojedas@usm.cl>
2023-05-31 00:30:32 -0400

    Commit_Bd

--- Software_Inscripcion/core/admin.py
+++ Software_Inscripcion/core/admin.py
@@ -3,4 +3,6 @@ from .models import Usuario
 
 # Register your models here.
 
-admin.site.register(Usuario)
+@admin.register(Usuario)  
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'rut', 'correo')  
